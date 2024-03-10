5930bf3703e4b9e3bc766bce95831c46a3fbf3c2
ripanRL <ripan.c@raftlabs.co>
2024-03-10 17:25:10 +0530

    feat: caching support

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initDB } from './mongodb/connection.service';
 import { ValidationPipe } from '@nestjs/common';
+import { connectToRedis } from './caching/redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env['PORT']);
   initDB();
+  connectToRedis();
 }
 bootstrap();
